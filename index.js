15ee035f7cd27ee3a957aa3c414af6d81aec5008
Alessia Colpo <99196359+AllyJeager@users.noreply.github.com>
2022-06-22 15:20:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,27 +14,35 @@ function scrollToTop() {
       cookie.style = "display:none;";
     }
 //citazioni
- const testimonialsContainer = document.querySelector('#testimonials-container')
-    const testimonial = document.querySelector('.testimonial')
-    const testimonials = [{
-        text: "&#8220; Lo studio: strumento per costruire la propria libertà,educazione dell'ingegno e della creatività al lavoro,ma soprattutto occasione privilegiata di capire la vita &#8221; <br> Enrico Palandri"
-      },
-      {
-        text: "&#8220; L'obiettivo non è vivere per sempre.L'obiettivo è creare qualcosa che vivrà per sempre &#8221; Chuck Palahniuk"
-      },
-    ]
-    let idx = 1
+ const testimonialsContainer = document.querySelector('.testimonials-container')
+const testimonial = document.querySelector('.testimonial')
 
-    function updateTestimonials() {
-      const {text} = testimonials [idx]
-      
-      testimonials.innerHTML = text
-      idx++
-      if (idx > testimonials.length - 1) {
-        idx = 0
-      }
-    }
-    setInterval(updateTestimonials, 6000)
+const testimonials = [
+  {
+    text:
+      "&#8220; Lo studio: strumento per costruire la propria libertà,educazione dell'ingegno e della creatività al lavoro,ma soprattutto occasione privilegiata di capire la vita &#8221; <br> Enrico Palandri",
+  },
+  {
+    text:
+      "&#8220; L'obiettivo non è vivere per sempre.L'obiettivo è creare qualcosa che vivrà per sempre &#8221; <br> Chuck Palahniuk",
+  },
+]
+
+let idx = 1
+
+function updateTestimonial() {
+  const { text } = testimonials[idx]
+
+  testimonial.innerHTML = text
+ 
+  idx++
+
+  if (idx > testimonials.length - 1) {
+    idx = 0
+  }
+}
+
+setInterval(updateTestimonial, 10000)
 
 //contatti
     const toggles = document.querySelectorAll('.faq-toggle')
